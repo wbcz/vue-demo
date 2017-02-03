e0778dfb7b8d80d6524a839e650e8b2571ac1d89
王波 <wangbo@taolx.com>
2017-02-03 16:50:16 +0800

    release发布

--- wb/server.js
+++ wb/server.js
@@ -1,31 +1,25 @@
 var http = require('http')
   , exec = require('exec')
 
-const PORT = 9988
-  , PATH = '../html'
-
-
 // var deployServer = http.createServer(function(request, response) {
 	var command = [
-    'cd  ..',
 		'git checkout new',
-		'git add ..',
+		'git add .',
 		'git commit -m "release发布"',
-		'git tag -a v1.5'
-		'git push origin v1.5'
-	]
+		'git tag -a v1.5 -m ""'
+		// 'git push origin v1.5'
+	].join(' && ')
 
-    exec(command, function(err, out, code) {
-    	console.log(111)
-      if (err instanceof Error) {
-        throw err
-      }
-      process.stderr.write(err)
-      process.stdout.write(out)
-      // response.writeHead(200)
-      // response.end('Deploy Done.')
+  exec(command, function(err, out, code) {
+    if (err instanceof Error) {
+      throw err
+    }
+    process.stderr.write(err)
+    process.stdout.write(out)
+    // response.writeHead(200)
+    // response.end('Deploy Done.')
 
-    })
+  })
 // })
 
 //deployServer.listen(PORT)
